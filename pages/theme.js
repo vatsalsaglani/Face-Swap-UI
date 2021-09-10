cd22c904d5aee68c15f77205bb20b054a28092df
vatsals <vatsals@quinnox.com>
2021-09-10 11:46:18 +0530

    defined values for dark and light mode properties

---
+++ pages/theme.js
@@ -0,0 +1,11 @@
+// pages/theme.js
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
