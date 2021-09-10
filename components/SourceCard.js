5233ec1ed77edc792c7f1a51dc96d6c67f31f026
vatsals <vatsals@quinnox.com>
2021-09-10 22:07:04 +0530

    added card condition

--- components/SourceCard.js
+++ components/SourceCard.js
@@ -1,5 +1,12 @@
 import { useState, useContext } from "react";
-import { Box, Center, Button, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Button,
+  Text,
+  useColorMode,
+  Image,
+} from "@chakra-ui/react";
 
 // contexts
 import GlobalContext from "../contexts/global";
@@ -8,11 +15,12 @@ import GlobalContext from "../contexts/global";
 import SourceMenuButton from "../components/SourceMenuButton";
 
 const SourceCard = () => {
-  const { setOpenImageGridModal } = useContext(GlobalContext);
+  const { setOpenImageGridModal, sourceImage } = useContext(GlobalContext);
   const { colorMode, toogleColorMode } = useColorMode();
   const openModal = () => {
     setOpenImageGridModal(true);
   };
+  console.log("Source Image: ", sourceImage);
   return (
     <Box
       minHeight="55vh"
@@ -36,18 +44,23 @@ const SourceCard = () => {
           Source Image
         </Text>
       </Box>
+      {sourceImage ? (
+        <Box pt="4px" pb="4px" pl="5px" pr="5px">
+          <Image
+            src={`data:image/png;base64,  ${sourceImage}`}
+            borderRadius="md"
+            _hover={{
+              cursor: "pointer",
+              // border: "2px blue",
+              // borderRadius: "md",
+              boxShadow: "2px 2px 6px #6d6d74,-2px -2px 6px #93939e",
+            }}
+          />
+        </Box>
+      ) : null}
+
       <Box pt="4px" pb="4px" mt="8px" mb="8px">
         <Center display="flex">
-          {/* <Button
-            variant="solid"
-            size="md"
-            colorScheme="facebook"
-            bgGradient="linear(to right, #224d8f,#d55191)"
-            // onClick={setOpenImageGridModal(true)}
-            onClick={openModal}
-          >
-            UPLOAD
-          </Button> */}
           <SourceMenuButton />
         </Center>
       </Box>
--- components/SourceMenuButton.js
+++ components/SourceMenuButton.js
@@ -25,22 +25,42 @@ import GlobalContext from "../contexts/global";
 import InputFileButton from "../components/InputFileButton";
 
 const SourceMenuButton = () => {
-  const { setSourceImage } = useContext(GlobalContext);
+  const { setSourceImage, setOpenCaptureModal, setOpenSourceCaptureModal } =
+    useContext(GlobalContext);
   const { colorMode } = useColorMode();
-  const fileRef = useRef();
-  // const
-  //   const chooseFile = () => {
-  //     const { current } = fileRef(current || { click: () => {} }).click();
-  //   };
-
-  const chooseFile = (fileRef) => {
-    fileRef.current.click();
-  };
 
   const setFile = (event) => {
     let file = event.target.files[0];
     console.log("File: ", file);
-    setSourceImage(file);
+    // setSourceImage(file);
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      // use a regex to remove data url part
+      const readerresult = reader.result;
+      const base64String = readerresult
+        .replace("data:", "")
+        .replace(/^.+,/, "");
+
+      // log to console
+      // logs wL2dvYWwgbW9yZ...
+      // console.log(base64String);
+      //   setShowSourceImage(readerresult);
+      setSourceImage(base64String);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const openImageCaptureModal = () => {
+    setOpenSourceCaptureModal(true);
+  };
+
+  const fs_gradient = {
+    dark: [
+      "linear(to-tr, teal.300,yellow.400)",
+      "linear(to-t, blue.200, teal.500)",
+      "linear(to-b, orange.100, purple.300)",
+    ],
+    light: "linear(to-l, #7928CA,#FF0080)",
   };
 
   return (
@@ -51,20 +71,27 @@ const SourceMenuButton = () => {
           variant="solid"
           size="md"
           colorScheme="facebook"
-          bgGradient="linear(to right, #224d8f,#d55191)"
+          //   bgGradient="linear(to right, #224d8f,#d55191)"
+          bgGradient={fs_gradient[colorMode]}
+          color={colorMode === "light" ? "gray.100" : "gray.900"}
+          _hover={{
+            bgGradient:
+              colorMode === "light"
+                ? fs_gradient["dark"]
+                : fs_gradient["light"],
+            color: colorMode === "light" ? "gray.100" : "gray.900",
+          }}
         >
           UPLOAD
         </MenuButton>
         <MenuList>
           <InputFileButton
-            // ref={fileRef}
-            // chooseFile={chooseFile}
             setFile={setFile}
             buttonTxt={"Select"}
             color={colorMode === "dark" ? "white" : "white"}
           />
           <MenuDivider />
-          <MenuItem>Capture</MenuItem>
+          <MenuItem onClick={openImageCaptureModal}>Capture</MenuItem>
         </MenuList>
       </Menu>
     </>
