f4733a0f45a59aed6bb8c06b214c55690b1f9e14
vatsals <vatsals@quinnox.com>
2021-09-11 12:57:01 +0530

    added font

--- styles/theme.js
+++ styles/theme.js
@@ -4,6 +4,9 @@ import { extendTheme } from "@chakra-ui/react";
 const config = {
   initialColorMode: "light",
   useSystemColorMode: false,
+  fonts: {
+    body: "Lobster",
+  },
 };
 
 const theme = extendTheme({ config });
