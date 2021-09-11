579efb025238c6c010a59b843a88522c1a094b95
vatsals <vatsals@quinnox.com>
2021-09-11 12:55:23 +0530

    added google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import { GlobalContextProvider } from "../contexts/global";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
+import "../css/main.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextProvider>
-      <ChakraProvider resetCSS>
+      <ChakraProvider theme={theme} resetCSS>
         <Component {...pageProps} />
       </ChakraProvider>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: "Roboto Mono", monospace;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </GlobalContextProvider>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,14 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
